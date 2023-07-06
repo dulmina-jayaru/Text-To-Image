52005d6381495073fee0af07ea841bdce77d8194
Adolf Sipes <112796306+sltechnolk@users.noreply.github.com>
2023-07-06 11:22:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
-from flask import Flask
-from googletrans import Translator
-
+import os
+import io
+import warnings
+import base64
+from PIL import Image
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+from flask import Flask, jsonify , request
 def create_app():
     app = Flask(__name__)
-
-    # Read API keys from apis.txt file
-    def read_api_keys():
-        api_keys = []
-        with open("apis.txt", "r") as file:
-            for line in file:
-                api_keys.append(line.strip())
-        return api_keys
-
-    # Check if the provided API key is valid
-    def is_valid_api_key(api_key):
-        api_keys = read_api_keys()
-        return api_key in api_keys
-
-    # API endpoint for processing requests
-    @app.route("/translate/<api_key>/<text>/<target_language>")
-    def translate_text(api_key, text, target_language):
-        # Check if the API key is valid
-        if not is_valid_api_key(api_key):
-            return "Invalid API key"
-
-        # Perform the translation
-        translator = Translator()
-        translation = translator.translate(text, dest=target_language).text
-
-        return translation
+    # Set up our connection to the API.
+    stability_api = client.StabilityInference(
+        key="sk-5yp9tp2hjPaf6Y9sepkYsEooHiWHWTP0Yp1WvNrKxHy4T8li",  # API Key reference.
+        verbose=True,  # Print debug messages.
+        engine="stable-diffusion-xl-1024-v0-9",
+    )
+    
+    
+    @app.route('/generate_image', methods=['GET'])
+    def generate_image():
+        prompt = request.args.get('prompt', '')  
+        # Set up our initial generation parameters.
+        answers = stability_api.generate(
+            prompt="expansive landscape rolling greens with blue daisies and yggdrasil under a blue alien sky, masterful, ghibli",
+            seed=992446758,
+            steps=50,
+            cfg_scale=8.0,
+            width=1024,
+            height=1024,
+            samples=1,
+            sampler=generation.SAMPLER_K_DPMPP_2M
+        )
+    
+        image_data = None
+    
+        for resp in answers:
+            for artifact in resp.artifacts:
+                if artifact.finish_reason == generation.FILTER:
+                    warnings.warn(
+                        "Your request activated the API's safety filters and could not be processed."
+                        "Please modify the prompt and try again."
+                    )
+                if artifact.type == generation.ARTIFACT_IMAGE:
+                    img = Image.open(io.BytesIO(artifact.binary))
+                    buffered = io.BytesIO()
+                    img.save(buffered, format="PNG")
+                    image_data = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    
+        return jsonify({'image_data': image_data})
 
     return app
 
